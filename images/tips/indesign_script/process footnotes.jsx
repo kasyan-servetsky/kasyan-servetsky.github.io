7918043a9a75a0b7fcf5d363260911ec0125286c
kasyan-servetsky <askoldich@yahoo.com>
2026-05-30 18:55:41 +0300

    Релиз сайта. Финальная чистая версия

---
+++ images/tips/indesign_script/process footnotes.jsx
@@ -0,0 +1,15 @@
+﻿var scriptName = "Process footnotes";
+
+app.doScript(main, ScriptLanguage.JAVASCRIPT, undefined, UndoModes.FAST_ENTIRE_SCRIPT, "\"" + scriptName + "\" Script");
+
+function main() {
+	var doc = app.activeDocument;
+	var story = app.selection[0].parentStory;
+	var footnotes = story.footnotes;
+	
+	for (var i = 0; i < footnotes.length; i++) {
+		var footnote = footnotes[i];
+		footnote.texts[0].applyParagraphStyle(doc.paragraphStyles.itemByName("Footnote"), true);
+	}
+
+}
